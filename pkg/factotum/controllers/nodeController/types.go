b1cc95cd64e1991f72d2b9829ce70af182d720eb
Russell Brown <rjbrown57@gmail.com>
2025-06-08 18:08:29 -0400

    remove watches from nc

--- pkg/factotum/controllers/nodeController/types.go
+++ pkg/factotum/controllers/nodeController/types.go
@@ -1,25 +1,20 @@
 package nodecontroller
 
 import (
-	"context"
 	"sync"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/rjbrown57/factotum/api/v1alpha1"
 	fc "github.com/rjbrown57/factotum/pkg/factotum"
 	fcHandlers "github.com/rjbrown57/factotum/pkg/factotum/handlers"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const controllerName = "nodeController"
 
 type NodeController struct {
 	K8sClient   *kubernetes.Clientset
-	Watcher     watch.Interface
 	MsgChan     chan NcMsg
 	Wg          *sync.WaitGroup
 	NodeConfigs map[string]*v1alpha1.NodeConfig // a cache for updates triggered by the watcher
@@ -50,18 +45,8 @@ func NewNodeController(k8sClient *kubernetes.Clientset) (*NodeController, error)
 
 	log.Info("Initializing", "Controller", controllerName)
 
-	// Set up a watch on the nodes in the cluster
-	watcher, err := k8sClient.CoreV1().Nodes().Watch(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		log.Error(err, "Error setting watch on nodes")
-		return nil, err
-	}
-
-	nc.Watcher = watcher
-
 	// Start watching for node events
 	debugLog.Info("Starting to WatchNodes routine")
-	go nc.Watch(watcher.ResultChan())
 
 	// Start the NodeApplier that will apply labels to nodes
 	debugLog.Info("Starting ApplyLabels routine")
