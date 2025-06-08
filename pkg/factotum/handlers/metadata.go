40345964ecbd421c986146b3a33736f4041a3bef
Russell Brown <rjbrown57@gmail.com>
2025-06-08 11:14:51 -0400

    refactor: adjust how metadata handler functions to account for nil maps

--- pkg/factotum/handlers/metadata.go
+++ pkg/factotum/handlers/metadata.go
@@ -11,11 +11,10 @@ type MetaDataHandler struct{}
 
 // MetaDataHandler will update the metadata of the object
 // based on the annotations and labels defined in the FactotumConfig
-// It will return true if any of the metadata was updated
 func (m *MetaDataHandler) Update(Object v1.Object, FactotumConfig factotum.Config) v1.Object {
 
-	_ = k8s.ProcessMetaDataMap(Object.GetAnnotations(), FactotumConfig.GetAnnotationSet())
-	_ = k8s.ProcessMetaDataMap(Object.GetLabels(), FactotumConfig.GetLabelSet())
+	Object.SetAnnotations(k8s.ProcessMetaDataMap(Object.GetAnnotations(), FactotumConfig.GetAnnotationSet()))
+	Object.SetLabels(k8s.ProcessMetaDataMap(Object.GetLabels(), FactotumConfig.GetLabelSet()))
 
 	return Object
 }
--- pkg/k8s/metdata_test.go
+++ pkg/k8s/metdata_test.go
@@ -11,56 +11,52 @@ func TestProcessMetaDataMap(t *testing.T) {
 		currentMap  map[string]string
 		desiredMap  map[string]string
 		expectedMap map[string]string
-		updated     bool
 	}{
 		{
 			name:        "Add new key-value pair",
 			currentMap:  map[string]string{"key1": "value1"},
 			desiredMap:  map[string]string{"key1": "value1", "key2": "value2"},
 			expectedMap: map[string]string{"key1": "value1", "key2": "value2"},
-			updated:     true,
 		},
 		{
 			name:        "Update existing key-value pair",
 			currentMap:  map[string]string{"key1": "value1"},
 			desiredMap:  map[string]string{"key1": "newValue"},
 			expectedMap: map[string]string{"key1": "newValue"},
-			updated:     true,
 		},
+
 		{
 			name:        "Remove key with empty value",
 			currentMap:  map[string]string{"key1": "value1", "key2": "value2"},
 			desiredMap:  map[string]string{"key1": "value1", "key2": ""},
 			expectedMap: map[string]string{"key1": "value1"},
-			updated:     true,
 		},
 		{
 			name:        "No changes needed",
 			currentMap:  map[string]string{"key1": "value1"},
 			desiredMap:  map[string]string{"key1": "value1"},
 			expectedMap: map[string]string{"key1": "value1"},
-			updated:     false,
 		},
 		{
 			name:        "Remove all keys",
 			currentMap:  map[string]string{"key1": "value1", "key2": "value2"},
 			desiredMap:  map[string]string{"key1": "", "key2": ""},
 			expectedMap: map[string]string{},
-			updated:     true,
+		},
+		{
+			name:        "Nil currentMap add new key-value pairs",
+			currentMap:  nil,
+			desiredMap:  map[string]string{"key1": "value1", "key2": "value2"},
+			expectedMap: map[string]string{"key1": "value1", "key2": "value2"},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			currentMap := tt.currentMap
-			updated := ProcessMetaDataMap(currentMap, tt.desiredMap)
+			returnedMap := ProcessMetaDataMap(tt.currentMap, tt.desiredMap)
 
-			if updated != tt.updated {
-				t.Errorf("expected updated to be %v, got %v", tt.updated, updated)
-			}
-
-			if !reflect.DeepEqual(currentMap, tt.expectedMap) {
-				t.Errorf("expected map to be %v, got %v", tt.expectedMap, currentMap)
+			if !reflect.DeepEqual(returnedMap, tt.expectedMap) {
+				t.Errorf("expected map to be %v, got %v", tt.expectedMap, returnedMap)
 			}
 		})
 	}
